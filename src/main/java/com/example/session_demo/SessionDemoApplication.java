c309258ffaaf95cb5d72f43b2b29f5ebb7179ac2
schreiberpaul <mail@schreiber-p.de>
2025-03-17 15:03:06 +0100

    Initial commit

---
+++ src/main/java/com/example/session_demo/SessionDemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.session_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SessionDemoApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionDemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SessionDemoApplication.class);
+	}
+
+	@RestController
+	public class TomcatController {
+		
+		@GetMapping("/hello")
+		public String sayHello() {
+			return "Hello, World";
+		}
+	}
+
+}
---
+++ src/test/java/com/example/session_demo/SessionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.session_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
